9df89647aef4112ae77e0f307c09ed539bcc85e1
dmitriivqa39 <dmivron620@gmail.com>
2025-11-23 01:24:44 +0500

    Popup todo list logic added

---
+++ popup/popup.js
@@ -0,0 +1,60 @@
+let tasks = [];
+
+const addTskBtn = document.getElementById('add-task-btn');
+addTskBtn.addEventListener('click', () => addTask());
+
+chrome.storage.sync.get(['tasks'], (res) => {
+  tasks = res.tasks ? res.tasks : [];
+  renderTasks();
+});
+
+function saveTasks() {
+  chrome.storage.sync.set({
+    tasks,
+  });
+}
+
+function renderTask(taskNum) {
+  const taskRow = document.createElement('div');
+  const text = document.createElement('input');
+  text.type = 'text';
+  text.placeholder = 'Enter a task...';
+  text.value = tasks[taskNum];
+  text.addEventListener('change', () => {
+    tasks[taskNum] = text.value;
+    saveTasks();
+  });
+
+  const deleteBtn = document.createElement('input');
+  deleteBtn.type = 'button';
+  deleteBtn.value = 'x';
+  deleteBtn.addEventListener('click', () => {
+    deleteTask(taskNum);
+  });
+
+  taskRow.appendChild(text);
+  taskRow.appendChild(deleteBtn);
+  const taskContainer = document.getElementById('task-container');
+  taskContainer.appendChild(taskRow);
+}
+
+function addTask() {
+  const tasksNum = tasks.length;
+  tasks.push('');
+  renderTask(tasksNum);
+  saveTasks();
+}
+
+function deleteTask(taskNum) {
+  tasks.splice(taskNum, 1);
+  renderTasks();
+  saveTasks();
+}
+
+function renderTasks() {
+  const taskContainer = document.getElementById('task-container');
+  taskContainer.textContent = '';
+  tasks.forEach((taskText, taskNum) => {
+    renderTask(taskNum);
+  });
+}
