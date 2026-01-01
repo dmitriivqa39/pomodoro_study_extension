9e3ea1aa7d97f2aada175d0267dbbb2b6f7f8563
dmitriivqa39 <dmivron620@gmail.com>
2026-01-01 16:46:08 +0500

    Notification added

--- background.js
+++ background.js
@@ -7,7 +7,15 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     chrome.storage.local.get(['timer', 'isRunning'], (res) => {
       if (res.isRunning) {
         let timer = res.timer + 1;
-        console.log(timer);
+        isRunning = true;
+        if (timer === 60 * 25) {
+          this.registration.showNotification('Pomodoro Timer', {
+            body: '25 minutes has passed!',
+            icon: './misc/icon.png',
+          });
+          timer = 0;
+          isRunning = false;
+        }
         chrome.storage.local.set({
           timer,
         });
